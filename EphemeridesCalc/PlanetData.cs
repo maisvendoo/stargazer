ab7ef7bd6ecf3830a3362cd224f56f2c2e12943f
maisvendoo <maisvendoo@gmail.com>
2015-10-06 11:19:27 +0300

    First code

---
+++ EphemeridesCalc/PlanetData.cs
@@ -0,0 +1,10 @@
+﻿public struct TOrbitData
+{
+    public double a;        // Orbit semimajor axis (m)
+    public double e;        // Orbit eccentricity
+    public double i;        // Orbit inclination (deg)
+    public double Omega;    // Longitude of the ascending node (deg)
+    public double omega;    // Periapsys argument (deg)
+    public double M0;       // Middle anomaly (rad)
+    public double t0;       // Epoch time (sec)
+}
